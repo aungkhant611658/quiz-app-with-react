5212e845780a2f4f47de6fb4b4f72fc410b06f1e
aungkhant611658 <aungkhant611658@gmail.com>
2024-04-16 18:20:34 +0630

    setup question and answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,32 @@
 import React, { useState } from "react";
+import Questions from "../questions";
 
 const Quiz = () => {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
 
-  return <p>Currently active Question</p>;
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{Questions[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {Questions[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
---
+++ src/questions.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: "q1",
+    text: "ALU stands for",
+    answers: [
+      "Arithmetic Logic Unit.",
+      "Array Logic Unit.",
+      "Application Logic Unit.",
+      "None of above.",
+    ],
+  },
+  {
+    id: "q2",
+    text: "The brain of any computer system is",
+    answers: ["CPU.", "ALU.", "Memory.", "Control Unit.", "None of above."],
+  },
+  {
+    id: "q3",
+    text: "What difference does the 5th generation computer have from other generation computers?",
+    answers: [
+      "All of the above",
+      "Technological advancement",
+      "Scientific code",
+      "Object Oriented Programming",
+      "None of the above",
+    ],
+  },
+  {
+    id: "q4",
+    text: "Which of the following computer generation uses concept of artificial intelligence?",
+    answers: [
+      "Forth Generation",
+      "First Generation",
+      "Second Generation",
+      "Third Generation",
+    ],
+  },
+  {
+    id: "q5",
+    text: "When a key is pressed on keyboard, which standard is used for converting the keystroke into the corresponding bits",
+    answers: ["ASCII.", "ANSI.", "EBCDIC.", "ISO."],
+  },
+  {
+    id: "q6",
+    text: "Which device is used as the standard pointing device in a Graphical User Environment",
+    answers: ["Mouse.", "Keyboard.", "Joystick.", "trackball."],
+  },
+  {
+    id: "q7",
+    text: "Which of the following is valid storage type?",
+    answers: [
+      "Pen Drive",
+      "CPU",
+      "Keyboard",
+      "Track Ball",
+      "None of the above",
+    ],
+  },
+  {
+    id: "q8",
+    text: "The section of the CPU that is responsible for performing mathematical operations",
+    answers: [
+      "ALU.",
+      "Memory.",
+      "Register Unit.",
+      "Control Unit.",
+      "None of the above",
+    ],
+  },
+  {
+    id: "q9",
+    text: "Any storage device added to computer beyond the immediately usable main storage is known as:",
+    answers: [
+      "Floppy disk",
+      "Hard Disk",
+      "Backing store",
+      "Punched Card",
+      "None of the above",
+    ],
+  },
+  {
+    id: "q10",
+    text: "The list of coded instructions is called",
+    answers: [
+      "Computer Program.",
+      "Algorithm.",
+      "Flowchart.",
+      "Utility Program.",
+      "None of the above",
+    ],
+  },
+];
